57c4119867cdfd1de2be1fba87f394f22faca40d
Luis Felipe Dussán Rueda <78167781+luisfelipedussan@users.noreply.github.com>
2025-01-13 22:29:20 -0500

    Primer commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { BookComponent } from './pages/book/book.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { MusicComponent } from './pages/music/music.component';
+import { ContactComponent } from './pages/contact/contact.component';
+//import { AIProyectComponent } from './pages/AIProyect/AIProyect.component';
+
+const routes: Routes = [
+
+  {path:'home',component: HomeComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'about',component:AboutComponent},
+  {path:'book',component:BookComponent},
+  {path:'shop',component:ShopComponent},
+  {path:'blog',component:BlogComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'music',component:MusicComponent}
+  //{path:'AIProyect',component:AIProyectComponent},
+
+
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './sharepage/navbar/navbar.component';
+import { FooterComponent } from './sharepage/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
+import { AboutComponent } from './pages/about/about.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ShopComponent } from './pages/shop/shop.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { BookComponent } from './pages/book/book.component';
+import { MusicComponent } from './pages/music/music.component';
+//import { AIProyectComponent } from './pages/AIProyect/AIProyect.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeComponent,
+    GalleryComponent,
+    AboutComponent,
+    ContactComponent,
+    ShopComponent,
+    BlogComponent,
+    BookComponent,
+    MusicComponent
+    //AIProyectComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
