0dd24ed0fb9e131f8cc833cd43da10e9a3f6fc2a
Luis Felipe Dussán Rueda <78167781+luisfelipedussan@users.noreply.github.com>
2025-01-18 12:04:34 -0500

    Updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -12,12 +12,13 @@ import { ContactComponent } from './pages/contact/contact.component';
 
 const routes: Routes = [
 
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   {path:'home',component: HomeComponent},
   {path:'gallery',component:GalleryComponent},
   {path:'about',component:AboutComponent},
-  {path:'book',component:BookComponent},
-  {path:'shop',component:ShopComponent},
-  {path:'blog',component:BlogComponent},
+  //{path:'book',component:BookComponent},
+  //{path:'shop',component:ShopComponent},
+  //{path:'blog',component:BlogComponent},
   {path:'contact',component:ContactComponent},
   {path:'music',component:MusicComponent}
   //{path:'AIProyect',component:AIProyectComponent},
